22e8ac2880944c8722f88476ce8950c58b182737
Knight-coder <72149616+Knight-coder@users.noreply.github.com>
2021-10-18 09:29:49 +0530

    Add files via upload
    THIS IS BUBLE SORT ALGORITHM

---
+++ Bubble Sort.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Bubble_Sort(int arr[], int size)
+{
+    int temp;
+    for (int round = 1; round <= size - 1; round++)
+    {
+        for (int i = 0; i <= size - round - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                //Swap
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size = 10;
+    int arr[size] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    Bubble_Sort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d \t", arr[i]);
+    }
+}
